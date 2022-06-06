eee83ecfb2eb9257bf92685fd9a104437def0f88
Ainur <ainurforex@mail.ru>
2022-06-06 17:37:17 +0500

    Урок 2 ДЗ 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Автоматическая типизация
+        var one=1;
+        //Целочисленные
+        byte two=2;
+        int three=30;
+        long four=4000000L;
+        //С плвающейточкой
+        float five=50.5555f;
+        double six=60.66666666666666666;
+        //Логический
+        boolean seven=true;
+
     }
 }
